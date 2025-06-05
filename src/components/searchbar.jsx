39630869678a5fa6a3d9ef80041848464bd8a377
MS-Kwak <kkminseo@gmail.com>
2025-06-05 11:08:54 +0900

    searchbar useEffect 수정

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -13,8 +13,7 @@ export default function Searchbar() {
 
   useEffect(() => {
     setSearch(q || '');
-  }),
-    [q];
+  }, [q]);
 
   const onChangeSearch = (e) => {
     setSearch(e.target.value);
